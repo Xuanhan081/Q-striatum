74bb7a4ba73a841b52d43c41dda007a691c9fb34
Xuanhan081 <melodymelody0801@gmail.com>
2025-05-27 18:38:11 +0800

    換成openVideoPopup

--- all.js
+++ all.js
@@ -4,7 +4,12 @@
 function openVideoPopup(src) {
         window.open(
         `${src}`,     // 網頁路徑（可換成任何網址）
-        "popupWindow",    // 視窗名稱
+        "Demo_Video",    // 視窗名稱
         "width=1280,height=720,scrollbars=yes,resizable=yes"
         );
     }
+
+const video = document.getElementById("Demo_Video");
+        video.addEventListener("ended", () => {
+          window.close(); // 播放完自動關閉視窗
+        });

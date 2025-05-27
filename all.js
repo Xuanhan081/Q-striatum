64bd657cd4e04962e77592cf01f63914081d9520
Xuanhan081 <melodymelody0801@gmail.com>
2025-05-27 18:21:04 +0800

    新增彈跳視窗JS

---
+++ all.js
@@ -0,0 +1,10 @@
+//自動生成tbody
+
+//彈跳視窗影片
+function openVideoPopup(src) {
+        window.open(
+        `${src}`,     // 網頁路徑（可換成任何網址）
+        "popupWindow",    // 視窗名稱
+        "width=1280,height=720,scrollbars=yes,resizable=yes"
+        );
+    }
